2c44b4a646938f104c8b81a579d224c19bdb2320
jinguangyang <498358505@qq.com>
2021-12-25 09:49:54 +0800

    INIT

---
+++ chinesetts/src/main/java/com/air4/chinesetts/dispatcher/OnTtsStateListener.java
@@ -0,0 +1,13 @@
+package com.air4.chinesetts.dispatcher;
+
+/**
+ * @author {@link "mailto:xuefeng.ding@outlook.com" "Xuefeng Ding"}
+ * Created 2020-07-28 14:25
+ */
+public interface OnTtsStateListener {
+    public void onTtsReady();
+
+    public void onTtsStart(String text);
+
+    public void onTtsStop();
+}
